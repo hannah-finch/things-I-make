49a507adb544417fa84a5e24566df9d8543e2b84
hannahschwen <hannahschwen@gmail.com>
2024-10-22 11:15:49 -0400

    art thing data shows on page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
 import { useRef, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useQuery } from '@apollo/client';
 import { ArrowDownIcon } from "@heroicons/react/24/outline";
 
+// import { GET_CRAFTS, GET_DESIGNS, GET_DEVS, GET_MUSICS } from "../utils/queries";
+import { GET_ARTS } from "../utils/queries";
+
+
 function HomePage() {
+  const { loading, data } = useQuery(GET_ARTS);
+  const artThings = data ? data.artThings : [];
+  // console.log(artThings)
+
   const devSection = useRef(null);
   const designSection = useRef(null);
   const artSection = useRef(null);
@@ -10,6 +19,14 @@ function HomePage() {
   const craftSection = useRef(null);
 
   const params = useParams();
+
+  // const { loadingCrafts, dataCrafts } = useQuery(GET_CRAFTS);
+  // const { loadingDesigns, dataDesigns } = useQuery(GET_DESIGNS);
+  // const { loadingDevs, dataDevs } = useQuery(GET_DEVS);
+  // const { loadingMusics, dataMusics } = useQuery(GET_MUSICS);
+
+
+
 
   useEffect(() => {
     const scrollSpot = params.section;
@@ -82,9 +99,17 @@ function HomePage() {
       <div className="test" ref={designSection} id="design-section">
         Design
       </div>
+
       <div className="test" ref={artSection} id="artSec">
         Art
+      {loading ? <h1>LOADING...</h1>
+      : <h1>{artThings[0].title}</h1>}
+
+
+{/* {artThings.title} */}
+        
       </div>
+
       <div className="test" ref={musicSection}>
         Music
       </div>
