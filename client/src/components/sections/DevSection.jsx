353a202d6c0d31a2048edff8a59ad42097bc69e9
hannahschwen <hannahschwen@gmail.com>
2024-11-25 18:08:58 -0500

    structure and style dev thing modal

--- client/src/components/sections/DevSection.jsx
+++ client/src/components/sections/DevSection.jsx
@@ -12,14 +12,92 @@ function DevSection(props) {
 
   const CardComponent = Card;
   const [selectedProject, setSelectedProject] = useState();
-  const cardsPerSlide = props.cardsPerSlide
-  const windowWidth = props.windowWidth
+  const cardsPerSlide = props.cardsPerSlide;
+  const windowWidth = props.windowWidth;
 
   function MoreSection({ thing }) {
     function handleClose() {
       setSelectedProject();
     }
-    // available from db: title, description, date, image
+
+    const Tech = () => {
+      if (thing.tech) {
+        const list = thing.tech.map((tech, index) => {
+          return (
+            <li key={index} className="text-bubble">
+              {tech}
+            </li>
+          );
+        });
+        return (
+          <>
+            <ul>{list}</ul>
+          </>
+        );
+      }
+    };
+
+    const Collab = () => {
+      if (thing.collab) {
+        const list = thing.collab.map((name, index) => {
+          return (
+            <p key={index} className="inline text-bubble-2">
+              {name}
+            </p>
+          );
+        });
+        return (
+          <>
+            <h4 className="inline">Collaborators:&nbsp; </h4> {list}
+          </>
+        );
+      }
+    };
+
+    const Buttons = ({ deployed, video, github }) => {
+      const GithubBtn = () => {
+        if (github) {
+          return (
+            <a className="link-3" href={github} target="_blank">
+              View on GitHub
+            </a>
+          );
+        }
+      };
+
+      const DeployBtn = () => {
+        if (deployed) {
+          return (
+            <a className="link-3" href={deployed} target="_blank">
+              Deployed Application
+            </a>
+          );
+        }
+      };
+
+      const VideoBtn = () => {
+        if (video) {
+          return (
+            <a className="link-3" href={video} target="_blank">
+              Video Demo
+            </a>
+          );
+        }
+      };
+
+      return (
+        <>
+          <div className="line-horizontal"></div>
+          <div className="btn-row">
+            <GithubBtn />
+            <DeployBtn />
+            <VideoBtn />
+          </div>
+        </>
+      );
+    };
+
+    // available from db: title, description, date, image, video, tech, collab, github, deployed
     return thing ? (
       <>
         <section className="modal-background">
@@ -35,7 +113,11 @@ function DevSection(props) {
                 <img src={thing.image}></img>
               </figure>
               <h1>{thing.title}</h1>
+              <div className="line-horizontal"></div>
+              <Tech></Tech>
               <p>{thing.description}</p>
+              <Collab></Collab>
+              <Buttons {...thing}></Buttons>
             </div>
             <div className="spacer"></div>
           </div>
